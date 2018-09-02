0628a26c3fd252364cbcaae94e85a12a08c33b90
Felipe2255 <39532299+Felipe2255@users.noreply.github.com>
2018-09-02 03:54:04 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#We define a makeCacheMatrix proc to initialize the object x (a matrix which
+#       inverse we might want), "inverse" (a variable where we would locate that
+#       inverse matrix) and define a list of functions set, get, setinv and
+#       getinv (to operate with that matrix x and it's inverse).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL # inverse starts uncalculated
+#set allows to define a new value for the matrix x in the parent environment of
+        #makeCacheMatrix
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+#get just expreses the value of x
+        get <- function() x
+#setinv assigns the value of the inverse of the setted x to the parent directory
+        setinv <- function(minverse) inverse <<- minverse
+#getinv retrieves, when invoked, the value of the inverse of x from the parent
+#       directory
+        getinv <- function() inverse
+#Creates a named list to allow manipulation of it's objects with the "$" notation
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+#The cacheSolve function actually calculates the inverse of the set matrix "x"
+#using the "solve" function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#       if the inverse variable is not null in the parent directory, then
+#       cacheSolve returns it's value
+        inverse <- x$getinv()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+#       if "inverse" is null, then cacheSolve calculates a new inverse getting the
+#       value of x with x$get, and then applying the solve function
+        data <- x$get()
+        inverse <- solve(data, ...)
+#       then it sets the new value of the inverse matrix to "inverse" variable
+#       and prints this result
+        x$setinv(inverse)
+        inverse
+}        
